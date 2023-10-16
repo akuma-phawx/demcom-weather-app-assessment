c3dd83c8aec07040fc649847085963622b9ad48c
Christoforos Vradis <christoforos.vradis@onecentral.nl>
2023-10-16 21:48:21 +0200

    feat(cities): load cities on a picker dropdown

--- screens/MainPage.js
+++ screens/MainPage.js
@@ -1,16 +1,21 @@
-import React from "react";
-import { View, Text, Button } from "react-native";
+import React, { useState } from "react";
+import { View } from "react-native";
+import { Picker } from "@react-native-picker/picker";
+
+const cities = ["Amsterdam", "Athens", "Paris", "Berlin"];
 
 function MainPage({ navigation }) {
+  const [city, setCity] = useState("Amsterdam");
   return (
     <View>
-      <Text>Main Page</Text>
-      <Button
-        title="Go to City Page"
-        onPress={() => {
-          navigation.navigate("CityPage");
-        }}
-      />
+      <Picker
+        selectedValue={city}
+        onValueChange={(itemValue) => setCity(itemValue)}
+      >
+        {cities.map((city, index) => (
+          <Picker.Item key={index} label={city} value={city}></Picker.Item>
+        ))}
+      </Picker>
     </View>
   );
 }
