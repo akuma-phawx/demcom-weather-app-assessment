b15490a9eb01d976431fd65e24c085b3b3304fb9
Christoforos Vradis <christoforos.vradis@onecentral.nl>
2023-10-17 13:25:24 +0200

    feat(tabs): add tabs in city page

--- screens/CityPage.js
+++ screens/CityPage.js
@@ -1,12 +1,9 @@
 import React from "react";
 import { View, Text } from "react-native";
+import Tabs from "../components/Tabs";
 
 // TODO: Create City Page
 export default function CityPage({ route }) {
   const { city } = route.params;
-  return (
-    <View>
-      <Text>Selected city: {city}</Text>
-    </View>
-  );
+  return <Tabs />;
 }
