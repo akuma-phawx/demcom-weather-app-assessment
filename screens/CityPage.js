94dc863cd07a7699eb3628ff23879779548bd753
Christoforos Vradis <christoforos.vradis@onecentral.nl>
2023-10-17 14:00:38 +0200

    feat(geo): add geocoding api

--- screens/CityPage.js
+++ screens/CityPage.js
@@ -1,9 +1,32 @@
-import React from "react";
-import { View, Text } from "react-native";
-import Tabs from "../components/Tabs";
+import React, { Fragment, useState, useEffect } from "react";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import HourlyWeatherScreen from "../screens/city/HourlyWeatherScreen";
+import SevenDayForecastScreen from "../screens/city/SevenDayForecastScreen";
 
-// TODO: Create City Page
+const Tab = createBottomTabNavigator();
+import { Text } from "react-native";
+import { getCityCoordinates } from "../services/api/useGeocodingApi";
+
 export default function CityPage({ route }) {
   const { city } = route.params;
-  return <Tabs />;
+  const [coordinates, setCoordinates] = useState({ lat: 0, lon: 0 });
+
+  useEffect(() => {
+    // Fetch the city coordinates when the component mounts
+    getCityCoordinates(city)
+      .then((coord) => setCoordinates(coord))
+      .catch((error) => console.error("Error fetching coordinates: ", error));
+  }, [city]);
+
+  return (
+    <Fragment>
+      <Text>
+        Coordinates: Lat: {coordinates.lat}, Lon: {coordinates.lon}
+      </Text>
+      <Tab.Navigator>
+        <Tab.Screen name="Hourly Weather" component={HourlyWeatherScreen} />
+        <Tab.Screen name="7 Day Forecast" component={SevenDayForecastScreen} />
+      </Tab.Navigator>
+    </Fragment>
+  );
 }
